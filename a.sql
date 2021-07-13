509dd0bd5b2da8921f34b3cbee4f2580ca1aff34
spafka <spafka@163.com>
2021-07-14 00:32:31 +0800

    first commit

---
+++ a.sql
@@ -0,0 +1,6 @@
+create table test004
+(
+    create_time timestamp not null,
+    modify_time timestamp not null
+);
+show create table test004;
---
+++ demo-service-auth/src/test/java/org/akazakov/keycloak/DemoServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akazakov.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
